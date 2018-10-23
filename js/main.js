c94ec2d20089bb261d51d78a0d02d4d73db2419a
Oana-Poran <oanaporan@gmail.com>
2018-10-23 12:11:38 +0300

    Role button added to link el. "View Details"

--- js/main.js
+++ js/main.js
@@ -180,8 +180,11 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute('role', 'button');
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
+
+// <a class="leaflet-control-zoom-out" href="#" title="Zoom out" role="button" aria-label="Zoom out">−</a>
 
   return li
 }
